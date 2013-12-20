23b617c33736e3948bec04893bd1b3f11afd3a74
Rémi Flamary <remi.flamary@gmail.com>
2013-12-20 09:17:58 +0100

    adding csv plugin

---
+++ plugins/csvload.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 26 13:41:48 2012
+
+@author: flam
+"""
+import datetime
+
+
+# name of the plugin
+plug_name='csvload'
+
+
+            
+def load_csv_files(files,sep):
+    
+    res=list()
+    for f in files:
+        res.append(load_csv(f,sep))
+    return res
+    
+def load_csv(fname,sep):
+    f = open(fname, 'r')
+
+    res=list()
+    
+    for line in f:
+        #print line
+        s=line.split(sep)
+        res.append(s)
+        
+        
+    f.close()
+    return res
+
+
+def plugin_return(config):
+    """
+    returns the plugin after website loading and store the data in ext['plugname'] for use in template
+    """
+    files=   config['Plugins'][plug_name]['files']
+    sep=config['Plugins'][plug_name]['sep']
+ 
+    res=load_csv_files(files,sep)
+
+    return res
+    
+def plugin_change_lists(website):
+    """
+    function that can modify the whole website (before content generation)
+    """
+    pass    
+    
+def plugin_change_lists_post(website):
+    """
+    function that can modify the whole website (after content generation)
+    """
+    pass    
+        
